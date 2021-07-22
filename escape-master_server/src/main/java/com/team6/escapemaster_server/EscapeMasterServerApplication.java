aa4c79dfbf40f38b83621043ce9caa1bd63cf117
Gu Yi <gu_yi163@163.com>
2021-07-22 10:53:25 +0800

    Initialize SpringBoot combined with MyBatis

---
+++ escape-master_server/src/main/java/com/team6/escapemaster_server/EscapeMasterServerApplication.java
@@ -0,0 +1,13 @@
+package com.team6.escapemaster_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeMasterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscapeMasterServerApplication.class, args);
+    }
+
+}
---
+++ escape-master_server/src/test/java/com/team6/escapemaster_server/EscapeMasterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.escapemaster_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeMasterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
