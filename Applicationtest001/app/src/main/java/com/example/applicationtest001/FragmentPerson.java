0d4fead0baee81af0550b1243ebe554f22508970
阿皮 <841544057@qq.com>
2021-07-25 12:59:56 +0800

    加了密室列表

--- Applicationtest001/app/src/main/java/com/example/applicationtest001/FragmentPerson.java
+++ Applicationtest001/app/src/main/java/com/example/applicationtest001/FragmentPerson.java
@@ -1,10 +1,12 @@
 package com.example.applicationtest001;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,16 @@ public class FragmentPerson extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_person,container,false);
+
+        Button btn1 = (Button)getActivity().findViewById(R.id.button13);
+
+        btn1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(getContext(),EditActivity.class);
+                startActivity(i);
+            }
+        });
+
         return v;
     }
 }
